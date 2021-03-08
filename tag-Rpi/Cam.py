26a357ce988a347c7c5e66b2b0684568c068596e
ayush <bishtayush0@gmail.com>
2021-03-08 04:01:56 -0800

    weird camera error

--- tag-Rpi/Cam.py
+++ tag-Rpi/Cam.py
@@ -6,8 +6,9 @@ camera = PiCamera()
 def take_pic():
     camera.start_preview()
     for i in range(3):
-        sleep(3)
+        sleep(2)
         camera.capture('/home/pi/Desktop/image%s.jpg' % i)
+        print("done")
     camera.stop_preview()
 
 take_pic()
--- tag-Rpi/ledTest.py
+++ tag-Rpi/ledTest.py
@@ -3,5 +3,7 @@ import time
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 GPIO.setup(18,GPIO.OUT)
-print "LED on"
+print("LED on")
 GPIO.output(18,GPIO.HIGH)
+print("LED off")
+GPIO.output(18,GPIO.LOW)
