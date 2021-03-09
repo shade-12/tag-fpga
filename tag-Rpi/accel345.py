722599414283792617c87c095e2f5d4cd86410c1
bisht10 <bishtayush0@gmail.com>
2021-03-09 14:23:20 -0800

    adding comments

--- tag-Rpi/accel345.py
+++ tag-Rpi/accel345.py
@@ -1,15 +1,17 @@
 import time
 import RPi.GPIO as GPIO
 import board
-import busio
+import busio # library to handle i2c serial protocol
 import adafruit_adxl34x
+#I2C interface
 
-i2c = busio.I2C(board.SCL, board.SDA)
-accelerometer = adafruit_adxl34x.ADXL345(i2c)
+i2c = busio.I2C(board.SCL, board.SDA) # make i2c connection with clock and data signal
+accelerometer = adafruit_adxl34x.ADXL345(i2c) # instanciate ADXL object
 
 def measure_speed():
     try:
         x,y,z = accelerometer.acceleration
+        # accelerometer.enable_motion_detection(threshold=18) #scale factor = 62.5 mg
         print("x-accel",x)
         print("y-accel",y)
         print("z-accel",z)
