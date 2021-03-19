9a4e33ce9e60a2255dd958a13d1c43586945f06e
bisht10 <bishtayush0@gmail.com>
2021-03-19 01:36:41 -0700

    test db

---
+++ tag-Rpi/DBtest.py
@@ -0,0 +1,74 @@
+import boto3
+
+# need these
+# aws_access_key_id = YOUR_ACCESS_KEY
+# aws_secret_access_key = YOUR_SECRET_KEY
+# region= REGION
+
+# Get the service resource.
+dynamodb = boto3.resource('dynamodb')
+
+# Create the DynamoDB table.
+table = dynamodb.create_table(
+    TableName='users',
+    KeySchema=[
+        {
+            'AttributeName': 'username',
+            'KeyType': 'HASH'
+        },
+        {
+            'AttributeName': 'last_name',
+            'KeyType': 'RANGE'
+        }
+    ],
+    AttributeDefinitions=[
+        {
+            'AttributeName': 'username',
+            'AttributeType': 'S'
+        },
+        {
+            'AttributeName': 'last_name',
+            'AttributeType': 'S'
+        },
+    ],
+    ProvisionedThroughput={
+        'ReadCapacityUnits': 5,
+        'WriteCapacityUnits': 5
+    }
+)
+
+print("creating table")
+
+# Wait until the table exists.
+table.meta.client.get_waiter('table_exists').wait(TableName='users')
+print("!!! created table !!!")
+print(table.creation_date_time)
+# Print out some data about the table.
+print(table.item_count)
+
+# Instantiate a table resource object
+table = dynamodb.Table('users')
+print(table.creation_date_time)
+
+table.put_item(
+   Item={
+        'username': 'janedoe',
+        'first_name': 'Jane',
+        'last_name': 'Doe',
+        'age': 25,
+        'account_type': 'standard_user',
+    }
+)
+
+print("inserted item")
+
+response = table.get_item(
+    Key={
+        'username': 'janedoe',
+        'last_name': 'Doe'
+    }
+)
+item = response['Item']
+
+print("fetch item")
+print(item)
