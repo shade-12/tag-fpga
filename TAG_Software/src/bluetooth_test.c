b90164de2c246e68fe155bb2f5fed293c94e8835
Shade Wong <shadeying@alumni.ubc.ca>
2021-03-19 17:25:28 -0700

    HC05 test

--- TAG_Software/src/bluetooth_test.c
+++ TAG_Software/src/bluetooth_test.c
@@ -3,14 +3,14 @@
 
 #include "bluetooth.h"
 
-void main(void)
-{
-    printf("UART Bluetooth Serial Port Test Program\n");
+#define IS_HC05_TEST 1
 
-    bluetoothInit();
-    bluetoothFlush();
-    printf("Flush done.\n");
 
+/**
+ * Test reading and writing single/multiple chars to the BT UART serial port
+ * by conecting the TX/RX pins with a jumper wire.
+ */
+void bt_pins_test() {
     // Put single char
     unsigned char d = bluetoothPutChar('A');
     printf("Put char: %c\n", d);
@@ -33,4 +33,44 @@ void main(void)
 
     printf("Exit.");
     return;
+}
+
+
+/**
+ * Test reading and writing single/multiple chars to the BT UART serial port
+ * by connecting RFS daughter board to GPIO 1 header pins on DE1,
+ * then establish connection btwn the HC05 module and mobile.
+ */
+void bt_slave_test() {
+
+    int ready;
+    unsigned char c;
+
+    while (1)
+    {
+        ready = bluetoothTestForReceivedData();
+        
+        if (ready) {
+            printf("Ready to read char: %d\n", ready);
+            c = bluetoothGetChar();
+            printf("Received: %c\n", c);
+        }
+    }
+    
+}
+
+void main(void)
+{
+    printf("UART Bluetooth Serial Port Test Program\n");
+
+    bluetoothInit();
+    bluetoothFlush();
+    printf("Flush done.\n");
+
+    #if (IS_HC05_TEST)
+        bt_slave_test();
+    #else
+        bt_pins_test();
+    #endif
+    
 }
