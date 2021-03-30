260e16ada407f505c577db4dfd89d48e7068f7e5
bisht10 <bishtayush0@gmail.com>
2021-03-30 13:35:46 -0700

    main

---
+++ tag-Rpi/temp_sensor.py
@@ -0,0 +1,32 @@
+import RPi.GPIO as GPIO
+import time
+import dht11
+
+import numpy as np
+
+# Set to BCM mode
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+# Set GPIO Pins
+GPIO_DHT = 18
+
+# Create a DHT11 instance
+ts = dht11.DHT11(pin = GPIO_DHT)
+
+
+# Get temperature in °C from DHT11
+def temp():
+    return ts.read().temperature
+    
+try:
+    print(temp())
+
+except KeyboardInterrupt:
+    print("\n")
+
+finally:
+    GPIO.cleanup()
+
+
+
+
