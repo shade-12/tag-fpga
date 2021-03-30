93533f434a627f9c239723c4d4752213ca6b023e
bisht10 <bishtayush0@gmail.com>
2021-03-30 13:34:59 -0700

    main

--- tag-Rpi/ledTest.py
+++
@@ -1,9 +0,0 @@
-import RPi.GPIO as GPIO
-import time
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
-GPIO.setup(18,GPIO.OUT)
-print("LED on")
-GPIO.output(18,GPIO.HIGH)
-print("LED off")
-GPIO.output(18,GPIO.LOW)
