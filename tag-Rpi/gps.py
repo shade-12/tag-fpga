51d129eba50035781adad33ee65669f8fca8be9d
bisht10 <bishtayush0@gmail.com>
2021-04-06 23:14:00 -0700

    gps

--- tag-Rpi/gps.py
+++ tag-Rpi/gps.py
@@ -35,30 +35,30 @@ def convert_to_degrees(raw_value):
     position = degrees + mm_mmmm
     position = "%.4f" %(position)
     return position
+
+def find_loc():
     
-
-
-gpgga_info = "$GPGGA,"
-ser = serial.Serial ("/dev/ttyS0")              #Open port with baud rate
-GPGGA_buffer = 0
-NMEA_buff = 0
-lat_in_degrees = 0
-long_in_degrees = 0
-
-try:
-    while True:
+    gpgga_info = "$GPGGA,"
+    ser = serial.Serial ("/dev/ttyS0")              #Open port with baud rate
+    GPGGA_buffer = 0
+    NMEA_buff = 0
+    lat_in_degrees = 0
+    long_in_degrees = 0
+    try:
         received_data = (str)(ser.readline())                   #read NMEA string received
         GPGGA_data_available = received_data.find(gpgga_info)   #check for NMEA GPGGA string                 
         if (GPGGA_data_available>0):
             GPGGA_buffer = received_data.split("$GPGGA,",1)[1]  #store data coming after "$GPGGA," string 
             NMEA_buff = (GPGGA_buffer.split(','))               #store comma separated data in buffer
             GPS_Info()                                          #get time, latitude, longitude
- 
+    
             print("lat in degrees:", lat_in_degrees," long in degree: ", long_in_degrees, '\n')
-            map_link = 'http://maps.google.com/?q=' + lat_in_degrees + ',' + long_in_degrees    #create link to plot location on Google map
-            print("<<<<<<<<press ctrl+c to plot location on google maps>>>>>>\n")               #press ctrl+c to plot on map and exit 
-            print("------------------------------------------------------------\n")
-                        
-except KeyboardInterrupt:
-    webbrowser.open(map_link)        #open current position information in google map
-    sys.exit(0)
+            return lat_in_degrees,long_in_degrees
+
+
+gpgga_info = "$GPGGA,"
+    ser = serial.Serial ("/dev/ttyS0")              #Open port with baud rate
+    GPGGA_buffer = 0
+    NMEA_buff = 0
+    lat_in_degrees = 0
+    long_in_degrees = 0
